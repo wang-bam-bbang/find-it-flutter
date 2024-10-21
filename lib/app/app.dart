5cfa77de6c2cc23174c850b63f3fe37bc5b1f7c1
2paperstar <lee@paperst.ar>
2024-10-22 00:03:01 +0900

    refactor: separate MaterialApp

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FindItApp extends StatelessWidget {
+  const FindItApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+import 'package:find_it/app/app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const FindItApp());
 }
