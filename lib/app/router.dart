559bbda1d91f91543d32c77b4c1e6686492f79e6
2paperstar <lee@paperst.ar>
2024-10-22 00:15:21 +0900

    feat: add app router

---
+++ lib/app/router.dart
@@ -0,0 +1,11 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:find_it/app/router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Page|Layout,Route')
+class FindItRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+        RedirectRoute(path: '/', redirectTo: '/splash'),
+        AutoRoute(path: '/splash', page: SplashRoute.page),
+      ];
+}
