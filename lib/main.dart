d35ef254a7b35f8d35a1c95d2ca653b238d831e9
2paperstar <lee@paperst.ar>
2024-10-22 00:35:41 +0900

    feat: init get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:find_it/app/app.dart';
+import 'package:find_it/app/di/locator.dart';
 import 'package:find_it/gen/strings.g.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
   LocaleSettings.useDeviceLocale();
   runApp(TranslationProvider(child: const FindItApp()));
 }
