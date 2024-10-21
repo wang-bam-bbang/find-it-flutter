22bd692f9495f63144e2e336614cc4b23e3dbe67
2paperstar <lee@paperst.ar>
2024-10-22 00:38:55 +0900

    feat: add login page and redirect to login page from splash page

--- lib/app/modules/splash/presentation/pages/splash_page.dart
+++ lib/app/modules/splash/presentation/pages/splash_page.dart
@@ -1,10 +1,24 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:find_it/app/router.gr.dart';
 import 'package:find_it/app/values/palette.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.router.replaceAll([const LoginRoute()]);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/app/router.dart
+++ lib/app/router.dart
@@ -7,5 +7,6 @@ class FindItRouter extends RootStackRouter {
   List<AutoRoute> get routes => [
         RedirectRoute(path: '/', redirectTo: '/splash'),
         AutoRoute(path: '/splash', page: SplashRoute.page),
+        AutoRoute(path: '/login', page: LoginRoute.page),
       ];
 }
