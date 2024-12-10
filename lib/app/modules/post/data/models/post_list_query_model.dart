8c2b4e4a53522de6f6ffe2c1dc4da49104450364
GanghyeonSeo <48593646+GanghyeonSeo@users.noreply.github.com>
2024-12-11 04:16:24 +0900

    feat: filter (#16)

--- lib/app/modules/post/data/models/post_list_query_model.dart
+++ lib/app/modules/post/data/models/post_list_query_model.dart
@@ -1,3 +1,4 @@
+import 'package:find_it/app/modules/post/domain/enums/item_category.dart';
 import 'package:find_it/app/modules/post/domain/enums/post_type.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -8,6 +9,7 @@ part 'post_list_query_model.g.dart';
 class PostListQueryModel with _$PostListQueryModel {
   const factory PostListQueryModel({
     required PostType type,
+    @JsonKey(includeIfNull: false) ItemCategory? category,
   }) = _PostListQueryModel;
 
   factory PostListQueryModel.fromJson(Map<String, dynamic> json) =>
--- lib/app/modules/post/domain/repositories/post_repository.dart
+++ lib/app/modules/post/domain/repositories/post_repository.dart
@@ -1,10 +1,12 @@
 import 'package:find_it/app/modules/post/domain/entities/post_creation_entity.dart';
 import 'package:find_it/app/modules/post/domain/entities/post_entity.dart';
 import 'package:find_it/app/modules/post/domain/entities/post_modification_entity.dart';
+import 'package:find_it/app/modules/post/domain/enums/item_category.dart';
 import 'package:find_it/app/modules/post/domain/enums/post_type.dart';
 
 abstract class PostRepository {
-  Future<List<PostEntity>> getPosts({required PostType type});
+  Future<List<PostEntity>> getPosts(
+      {required PostType type, ItemCategory? category});
   Future<List<PostEntity>> getMyPosts({required PostType type});
   Future<PostEntity> createPost({required PostCreationEntity post});
   Future<PostEntity> modifyPost(
